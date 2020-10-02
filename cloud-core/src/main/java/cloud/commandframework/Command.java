0873a9bea522755f7e0987bee1054855a4fef168
broccolai <me@broccol.ai>
2020-10-02 00:12:20 +0000

    Fix checkstyle issues

--- cloud-core/src/main/java/cloud/commandframework/Command.java
+++ cloud-core/src/main/java/cloud/commandframework/Command.java
@@ -38,7 +38,13 @@ import io.leangen.geantyref.TypeToken;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
