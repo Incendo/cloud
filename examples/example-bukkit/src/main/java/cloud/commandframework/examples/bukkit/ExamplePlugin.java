1080b1a2f0c7f6fcd220dd61e2ac8c3d7e879ac6
broccolai <me@broccol.ai>
2020-10-06 19:40:28 +0100

    Update example plugin for new compound argument mapper

--- examples/example-bukkit/src/main/java/cloud/commandframework/examples/bukkit/ExamplePlugin.java
+++ examples/example-bukkit/src/main/java/cloud/commandframework/examples/bukkit/ExamplePlugin.java
@@ -207,7 +207,8 @@ public final class ExamplePlugin extends JavaPlugin {
                                             TypeToken.get(Vector.class),
                                             Triplet.of("x", "y", "z"),
                                             Triplet.of(Integer.class, Integer.class, Integer.class),
-                                            triplet -> new Vector(triplet.getFirst(), triplet.getSecond(), triplet.getThird()),
+                                            (sender, triplet) -> new Vector(triplet.getFirst(), triplet.getSecond(),
+                                                                            triplet.getThird()),
                                             Description.of("Coordinates"))
                                     .handler(context -> manager.taskRecipe().begin(context)
                                              .synchronous(commandContext -> {
