5f42f792aa40853a097fafef9f4c965ee2b96de6
Alexander Söderberg <sauilitired@gmail.com>
2020-10-22 07:22:35 +0200

    :books: Change `false` to `true` in method javadoc

--- cloud-core/src/main/java/cloud/commandframework/LockableCommandManager.java
+++ cloud-core/src/main/java/cloud/commandframework/LockableCommandManager.java
@@ -70,7 +70,7 @@ public abstract class LockableCommandManager<C> extends CommandManager<C> {
     /**
      * {@inheritDoc}
      * <p>
-     * This should only be called when {@link #isCommandRegistrationAllowed()} is {@code false},
+     * This should only be called when {@link #isCommandRegistrationAllowed()} is {@code true},
      * else {@link IllegalStateException} will be called
      *
      * @param command Command to register
