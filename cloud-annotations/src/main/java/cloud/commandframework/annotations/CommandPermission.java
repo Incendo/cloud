46e4521050ab1b37bf28ef751aedd78280997784
jmp <jasonpenilla2@me.com>
2020-10-21 23:51:56 -0700

    ✨ Allow for class annotations as a default for when an annotation is not present on a method

--- cloud-annotations/src/main/java/cloud/commandframework/annotations/CommandPermission.java
+++ cloud-annotations/src/main/java/cloud/commandframework/annotations/CommandPermission.java
@@ -31,7 +31,7 @@ import java.lang.annotation.Target;
 /**
  * Equivalent to {@link cloud.commandframework.Command.Builder#permission(String)}
  */
-@Target(ElementType.METHOD)
+@Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CommandPermission {
 
