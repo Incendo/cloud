e0216dec9f55fd67fd9e41b46da367051d4a5302
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2020-12-14 19:58:29 +0100

    Add support for dashes and underscores in annotations parser syntax
    Also added a test for the parser, as it was missing
    
    Signed-off-by: Irmo van den Berge <irmo.vandenberge@ziggo.nl>

--- cloud-annotations/src/main/java/cloud/commandframework/annotations/SyntaxParser.java
+++ cloud-annotations/src/main/java/cloud/commandframework/annotations/SyntaxParser.java
@@ -38,11 +38,11 @@ import java.util.regex.Pattern;
  */
 final class SyntaxParser implements Function<@NonNull String, @NonNull List<@NonNull SyntaxFragment>> {
 
-    private static final Predicate<String> PATTERN_ARGUMENT_LITERAL = Pattern.compile("([A-Za-z0-9]+)(|([A-Za-z0-9]+))*")
+    private static final Predicate<String> PATTERN_ARGUMENT_LITERAL = Pattern.compile("([A-Za-z0-9\\-_]+)(|([A-Za-z0-9\\-_]+))*")
             .asPredicate();
-    private static final Predicate<String> PATTERN_ARGUMENT_REQUIRED = Pattern.compile("<([A-Za-z0-9]+)>")
+    private static final Predicate<String> PATTERN_ARGUMENT_REQUIRED = Pattern.compile("<([A-Za-z0-9\\-_]+)>")
             .asPredicate();
-    private static final Predicate<String> PATTERN_ARGUMENT_OPTIONAL = Pattern.compile("\\[([A-Za-z0-9]+)]")
+    private static final Predicate<String> PATTERN_ARGUMENT_OPTIONAL = Pattern.compile("\\[([A-Za-z0-9\\-_]+)]")
             .asPredicate();
 
     @Override
