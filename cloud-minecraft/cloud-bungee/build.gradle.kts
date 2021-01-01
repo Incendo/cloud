366dd192cb7699cda2acb9d0abe6eeb41226e396
jmp <jasonpenilla2@me.com>
2020-12-31 22:09:31 -0800

    Use an Object with fields instead of a Map for dependency versions

--- cloud-minecraft/cloud-bungee/build.gradle.kts
+++ cloud-minecraft/cloud-bungee/build.gradle.kts
@@ -1,4 +1,4 @@
 dependencies {
     api(project(":cloud-core"))
-    compileOnly("net.md-5", "bungeecord-api", versions["bungeecord"])
+    compileOnly("net.md-5", "bungeecord-api", Versions.bungeecord)
 }
