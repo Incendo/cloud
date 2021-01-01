558b814056dee86c2eca6b20140b5df5a9b236d7
jmp <jasonpenilla2@me.com>
2020-12-31 21:40:40 -0800

    build.gradle.kts buildscript{} -> buildSrc/build.gradle.kts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,5 +3,11 @@ plugins {
 }
 
 repositories {
+    mavenCentral()
+    maven("https://oss.sonatype.org/content/repositories/snapshots/")
     jcenter()
+}
+
+dependencies {
+    implementation("com.bmuschko", "gradle-nexus-plugin", "2.3.1")
 }
