b77679041d29049aea2ee3f3115f29782589bf38
Zach Levis <zml@aoeu.xyz>
2021-01-03 14:25:41 -0800

    fabric: Begin adding support for some wrapped vanilla arguments

--- cloud-minecraft/cloud-fabric/build.gradle.kts
+++ cloud-minecraft/cloud-fabric/build.gradle.kts
@@ -1,8 +1,10 @@
+import net.ltgt.gradle.errorprone.errorprone
+
 plugins {
     id("fabric-loom") version "0.5-SNAPSHOT"
 }
 
-// Set up a testmod source set
+/* set up a testmod source set */
 val testmod by sourceSets.creating {
     val main = sourceSets.main.get()
     compileClasspath += main.compileClasspath
@@ -18,32 +20,45 @@ val testmodJar by tasks.creating(Jar::class) {
 
 loom.unmappedModCollection.from(testmodJar)
 
-tasks.withType(ProcessResources::class).configureEach {
-    inputs.property("version", project.version)
-    filesMatching("fabric.mod.json") {
-        expand("version" to project.version)
+/* end of testmod setup */
+
+tasks {
+    compileJava {
+        options.errorprone {
+            excludedPaths.set(".*[/\\\\]mixin[/\\\\].*")
+        }
+    }
+
+    withType(ProcessResources::class).configureEach {
+        inputs.property("version", project.version)
+        filesMatching("fabric.mod.json") {
+            expand("version" to project.version)
+        }
+    }
+
+    withType(Javadoc::class).configureEach {
+        (options as? StandardJavadocDocletOptions)?.apply {
+            links("https://maven.fabricmc.net/docs/yarn-${Versions.fabricMc}+build.${Versions.fabricYarn}/")
+        }
     }
 }
 
-tasks.withType(Javadoc::class).configureEach {
-    // options.links("https://maven.fabricmc.net/docs/yarn-${vers["fabricMc"]}+build.${vers["fabricYarn"]}/")
-}
 
 dependencies {
-    minecraft("com.mojang:minecraft:${Versions.fabricMc}")
-    mappings("net.fabricmc:yarn:${Versions.fabricMc}+build.${Versions.fabricYarn}:v2")
-    modImplementation("net.fabricmc:fabric-loader:${Versions.fabricLoader}")
+    minecraft("com.mojang", "minecraft", Versions.fabricMc)
+    mappings("net.fabricmc", "yarn", "${Versions.fabricMc}+build.${Versions.fabricYarn}", classifier = "v2")
+    modImplementation("net.fabricmc", "fabric-loader", Versions.fabricLoader)
     modImplementation(fabricApi.module("fabric-command-api-v1", Versions.fabricApi))
 
     api(include(project(":cloud-core"))!!)
     implementation(include(project(":cloud-brigadier"))!!)
 
     include(project(":cloud-services"))
-    include("io.leangen.geantyref:geantyref:${Versions.geantyref}")
+    include("io.leangen.geantyref", "geantyref", Versions.geantyref)
 }
 
 publishing.publications.named("mavenJava", MavenPublication::class) {
-    // add all the jars that should be included when publishing to maven
+    /* add all the jars that should be included when publishing to maven */
     artifact(tasks.remapJar) {
         builtBy(tasks.remapJar)
     }
@@ -51,8 +66,8 @@ publishing.publications.named("mavenJava", MavenPublication::class) {
         builtBy(tasks.remapSourcesJar)
     }
 
-    // Loom is broken with project dependencies in the same build (because it resolves dependencies during configuration)
-    // Please look away
+    /* Loom is broken with project dependencies in the same build (because it resolves dependencies during configuration)
+     * Please look away */
     pom {
         withXml {
             val dependencies = asNode().appendNode("dependencies")
