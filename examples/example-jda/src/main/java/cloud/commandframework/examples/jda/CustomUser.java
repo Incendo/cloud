bf185d0ceaf60222bdb604690945b70ef48fd14d
p5nbTgip0r <cb3f02c5@opayq.com>
2021-01-05 02:37:53 -0800

    Adjust JDA example to provide `MessageReceivedEvent` in commandSender mappers.
    This is necessary for the `ChannelArgument` parser to work.

--- examples/example-jda/src/main/java/cloud/commandframework/examples/jda/CustomUser.java
+++ examples/example-jda/src/main/java/cloud/commandframework/examples/jda/CustomUser.java
@@ -25,22 +25,42 @@ package cloud.commandframework.examples.jda;
 
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+import java.util.Optional;
 
 public abstract class CustomUser {
 
+    private final MessageReceivedEvent event;
     private final User user;
     private final MessageChannel channel;
 
     /**
      * Construct a user
      *
+     * @param event   The message received event
      * @param user    Sending user
      * @param channel Channel that the message was sent in
      */
-    protected CustomUser(final @NonNull User user, final @NonNull MessageChannel channel) {
+    protected CustomUser(
+            final @Nullable MessageReceivedEvent event,
+            final @NonNull User user,
+            final @NonNull MessageChannel channel
+    ) {
+        this.event = event;
         this.user = user;
         this.channel = channel;
+    }
+
+    /**
+     * Get the message received event
+     *
+     * @return Optional of the message received event
+     */
+    public final @NonNull Optional<MessageReceivedEvent> getEvent() {
+        return Optional.ofNullable(this.event);
     }
 
     /**
