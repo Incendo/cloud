001eef0817fdb3cf70ed5d0f88d114949b415013
Alexander Söderberg <4096670+Citymonstret@users.noreply.github.com>
2021-01-14 07:31:53 +0100

    :sparkles: Add injection services (#211)
    * :sparkles: Add injection services
    
    * Change dependency scope for Guice

--- cloud-core/build.gradle.kts
+++ cloud-core/build.gradle.kts
@@ -1,5 +1,7 @@
 dependencies {
     api(project(":cloud-services"))
+    compileOnly("com.google.inject", "guice", Versions.guice)
     testImplementation("org.openjdk.jmh", "jmh-core", Versions.jmh)
     testImplementation("org.openjdk.jmh", "jmh-generator-annprocess", Versions.jmh)
+    testImplementation("com.google.inject", "guice", Versions.guice)
 }
---
+++ cloud-core/src/main/java/cloud/commandframework/annotations/injection/InjectionService.java
@@ -0,0 +1,43 @@
+//
+// MIT License
+//
+// Copyright (c) 2020 Alexander Söderberg & Contributors
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+package cloud.commandframework.annotations.injection;
+
+import cloud.commandframework.annotations.AnnotationAccessor;
+import cloud.commandframework.context.CommandContext;
+import cloud.commandframework.services.types.Service;
+import cloud.commandframework.types.tuples.Triplet;
+
+/**
+ * Service that can be registered to the {@link ParameterInjectorRegistry} in order to provide
+ * custom injection support. This can be used to integrate the Cloud with existing dependency
+ * injection frameworks.
+ *
+ * @param <C> Command sender type
+ * @since 1.4.0
+ */
+@FunctionalInterface
+public interface InjectionService<C> extends
+        Service<Triplet<CommandContext<C>, Class<?>, AnnotationAccessor>, Object> {
+
+}
