a7697c7e78d4a5c203c3b693404c799355741cd7
Alexander Söderberg <contact@alexander-soderberg.com>
2021-01-14 08:31:15 +0100

    Change dependency scope for Guice

--- cloud-core/build.gradle.kts
+++ cloud-core/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
     api(project(":cloud-services"))
-    implementation("com.google.inject", "guice", Versions.guice)
+    compileOnly("com.google.inject", "guice", Versions.guice)
     testImplementation("org.openjdk.jmh", "jmh-core", Versions.jmh)
     testImplementation("org.openjdk.jmh", "jmh-generator-annprocess", Versions.jmh)
+    testImplementation("com.google.inject", "guice", Versions.guice)
 }
