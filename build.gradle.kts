21fa35a9fce253157d753d2de9b0d914387675b6
jmp <jasonpenilla2@me.com>
2021-01-18 02:06:10 -0800

    Bump version to 1.5.0-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ gradle.taskGraph.whenReady {
 }
 
 group = "cloud.commandframework"
-version = "1.4.0"
+version = "1.5.0-SNAPSHOT"
 description = "Command framework and dispatcher for the JVM"
 
 subprojects {
