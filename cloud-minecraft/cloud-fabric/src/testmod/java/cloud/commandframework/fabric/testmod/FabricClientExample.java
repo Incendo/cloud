9e387074e150236474a0e1054e059423413e5258
jmp <jasonpenilla2@me.com>
2021-03-11 02:19:47 -0800

    fabric: Add a couple more commands to the test mod

--- cloud-minecraft/cloud-fabric/src/testmod/java/cloud/commandframework/fabric/testmod/FabricClientExample.java
+++ cloud-minecraft/cloud-fabric/src/testmod/java/cloud/commandframework/fabric/testmod/FabricClientExample.java
@@ -23,26 +23,106 @@
 //
 package cloud.commandframework.fabric.testmod;
 
+import cloud.commandframework.Command;
 import cloud.commandframework.arguments.standard.StringArgument;
 import cloud.commandframework.execution.CommandExecutionCoordinator;
 import cloud.commandframework.fabric.FabricClientCommandManager;
+import cloud.commandframework.meta.CommandMeta;
+import com.google.gson.JsonObject;
+import com.google.gson.internal.Streams;
+import com.google.gson.stream.JsonWriter;
+import com.mojang.brigadier.CommandDispatcher;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v1.FabricClientCommandSource;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.SaveLevelScreen;
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
+import net.minecraft.client.realms.gui.screen.RealmsBridgeScreen;
+import net.minecraft.command.CommandSource;
+import net.minecraft.command.argument.ArgumentTypes;
+import net.minecraft.text.ClickEvent;
 import net.minecraft.text.LiteralText;
+import net.minecraft.text.TranslatableText;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Instant;
 
 public final class FabricClientExample implements ClientModInitializer {
+
     @Override
     public void onInitializeClient() {
         final FabricClientCommandManager<FabricClientCommandSource> commandManager =
                 FabricClientCommandManager.createNative(CommandExecutionCoordinator.simpleCoordinator());
 
-        commandManager.command(
-                commandManager.commandBuilder("cloud_client")
-                        .literal("say")
-                        .argument(StringArgument.greedy("message"))
-                        .handler(ctx -> ctx.getSender().sendFeedback(
-                                new LiteralText("Cloud client commands says: " + ctx.get("message"))
-                        ))
-        );
+        final Command.Builder<FabricClientCommandSource> base = commandManager.commandBuilder("cloud_client");
+
+        commandManager.command(base.literal("dump")
+                .meta(CommandMeta.DESCRIPTION, "Dump the client's Brigadier command tree")
+                .handler(ctx -> {
+                    final Path target = FabricLoader.getInstance().getGameDir().resolve(
+                            "cloud-dump-" + Instant.now().toString().replace(':', '-') + ".json"
+                    );
+                    ctx.getSender().sendFeedback(
+                            new LiteralText("Dumping command output to ")
+                                    .append(new LiteralText(target.toString())
+                                            .styled(s -> s.withClickEvent(new ClickEvent(
+                                                    ClickEvent.Action.OPEN_FILE,
+                                                    target.toAbsolutePath().toString()
+                                            ))))
+                    );
+
+                    try (BufferedWriter writer = Files.newBufferedWriter(target); JsonWriter json = new JsonWriter(writer)) {
+                        final CommandDispatcher<CommandSource> dispatcher = MinecraftClient.getInstance()
+                                .getNetworkHandler()
+                                .getCommandDispatcher();
+                        final JsonObject object = ArgumentTypes.toJson(dispatcher, dispatcher.getRoot());
+                        json.setIndent("  ");
+                        Streams.write(object, json);
+                    } catch (final IOException ex) {
+                        ctx.getSender().sendError(new LiteralText(
+                                "Unable to write file, see console for details: " + ex.getMessage()
+                        ));
+                    }
+                }));
+
+        commandManager.command(base.literal("say")
+                .argument(StringArgument.greedy("message"))
+                .handler(ctx -> ctx.getSender().sendFeedback(
+                        new LiteralText("Cloud client commands says: " + ctx.get("message"))
+                )));
+
+        commandManager.command(base.literal("quit")
+                .handler(ctx -> {
+                    final MinecraftClient client = MinecraftClient.getInstance();
+                    disconnectClient(client);
+                    client.scheduleStop();
+                }));
+
+        commandManager.command(base.literal("disconnect")
+                .handler(ctx -> disconnectClient(MinecraftClient.getInstance())));
     }
+
+    private static void disconnectClient(final @NonNull MinecraftClient client) {
+        boolean singlePlayer = client.isInSingleplayer();
+        client.world.disconnect();
+        if (singlePlayer) {
+            client.disconnect(new SaveLevelScreen(new TranslatableText("menu.savingLevel")));
+        } else {
+            client.disconnect();
+        }
+        if (singlePlayer) {
+            client.openScreen(new TitleScreen());
+        } else if (client.isConnectedToRealms()) {
+            new RealmsBridgeScreen().switchToRealms(new TitleScreen());
+        } else {
+            client.openScreen(new MultiplayerScreen(new TitleScreen()));
+        }
+    }
+
 }
