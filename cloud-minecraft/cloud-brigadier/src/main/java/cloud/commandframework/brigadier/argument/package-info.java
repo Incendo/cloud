e01092ff23fd02216ad9f2689122b02970d3741d
jmp <jasonpenilla2@me.com>
2021-03-12 14:02:47 -0800

    brigadier: add `@since` tags

--- cloud-minecraft/cloud-brigadier/src/main/java/cloud/commandframework/brigadier/argument/package-info.java
+++ cloud-minecraft/cloud-brigadier/src/main/java/cloud/commandframework/brigadier/argument/package-info.java
@@ -25,5 +25,7 @@
 /**
  * Support for wrapping brigadier {@link com.mojang.brigadier.arguments.ArgumentType ArgumentTypes}
  * as Cloud {@link cloud.commandframework.arguments.parser.ArgumentParser}.
+ *
+ * @since 1.5.0
  */
 package cloud.commandframework.brigadier.argument;
