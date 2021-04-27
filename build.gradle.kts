c80cd64b345dbfd32da160c9a771810733965440
jmp <jasonpenilla2@me.com>
2021-04-27 15:37:26 -0700

    remove space

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
     id("net.kyori.indra.checkstyle") version indraVersion apply false
     id("net.kyori.indra.publishing.sonatype") version indraVersion apply false
     id("net.kyori.indra.license-header") version indraVersion apply false
-    id ("org.cadixdev.licenser") version "0.6.0-SNAPSHOT" apply false
+    id("org.cadixdev.licenser") version "0.6.0-SNAPSHOT" apply false
     id("com.github.johnrengelman.shadow") version "6.1.0" apply false
     id("net.ltgt.errorprone") version "1.3.0" apply false
     id("com.github.ben-manes.versions") version "0.36.0"
