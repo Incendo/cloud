26bdd9ad50ebaa39ebf9ae6f29a5f14d7bbba3fd
jmp <jasonpenilla2@me.com>
2021-04-27 16:00:05 -0700

    sponge: Fix license header violations

--- cloud-minecraft/cloud-sponge/src/main/java/cloud/commandframework/sponge/SpongeCaptionKeys.java
+++ cloud-minecraft/cloud-sponge/src/main/java/cloud/commandframework/sponge/SpongeCaptionKeys.java
@@ -21,7 +21,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 //
-
 package cloud.commandframework.sponge;
 
 import cloud.commandframework.captions.Caption;
