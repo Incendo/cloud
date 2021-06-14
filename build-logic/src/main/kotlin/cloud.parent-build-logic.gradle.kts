6cd674dc8fe616fe0a5ab06d466cb6818d0befcb
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2021-06-14 05:48:16 -0700

    build: Remove unused publishing stuff

--- build-logic/src/main/kotlin/cloud.parent-build-logic.gradle.kts
+++ build-logic/src/main/kotlin/cloud.parent-build-logic.gradle.kts
@@ -1,10 +1,3 @@
 plugins {
     id("net.kyori.indra.publishing.sonatype")
 }
-
-System.getenv("SNAPSHOT_PUBLISHING_USERNAME")?.run {
-    setProperty("incendoUsername", this)
-}
-System.getenv("SNAPSHOT_PUBLISHING_PASSWORD")?.run {
-    setProperty("incendoPassword", this)
-}
