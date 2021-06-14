f67b126cc9e8ddd9e340f43d5a0d4c90a528b425
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2021-06-14 06:07:13 -0700

    Also remove the dependency

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -8,7 +8,6 @@ repositories {
 
 dependencies {
     implementation(libs.indraCommon)
-    implementation(libs.indraPublishingSonatype)
     implementation(libs.testLoggerPlugin)
     implementation(libs.errorpronePlugin)
     implementation(libs.licenser)
