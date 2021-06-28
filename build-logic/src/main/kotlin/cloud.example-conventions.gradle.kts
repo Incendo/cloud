54f281abe77c430400d8b58f79fca6df5fc80471
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2021-06-28 00:51:34 -0700

    build: Remove unused extension

--- build-logic/src/main/kotlin/cloud.example-conventions.gradle.kts
+++ build-logic/src/main/kotlin/cloud.example-conventions.gradle.kts
@@ -2,8 +2,6 @@ plugins {
     id("cloud.base-conventions")
 }
 
-extensions.create<CloudExampleExtension>("cloudExample")
-
 // Only compile examples on CI, or when the compile-examples property exists
 if (!ci.get() && !compileExamples) {
     tasks.configureEach {
