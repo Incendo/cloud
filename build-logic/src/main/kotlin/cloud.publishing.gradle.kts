13948a3502b7933eb607157e3cd126f25af65df6
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2021-07-05 02:37:54 -0700

    Don't publish example modules

---
+++ build-logic/src/main/kotlin/cloud.publishing.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("cloud.base-conventions")
+    id("net.kyori.indra.publishing")
+}
+
+signing {
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
+}
