fa118f83056fe0e930ab9dec2bef4c915fd79387
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2021-09-16 10:38:44 -0700

    build: Update Dokka to 1.5.30

--- cloud-kotlin-extensions/build.gradle.kts
+++ cloud-kotlin-extensions/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.dokka.gradle.DokkaTask
 
 plugins {
     kotlin("jvm") version "1.5.30"
-    id("org.jetbrains.dokka") version "1.4.20"
+    id("org.jetbrains.dokka") version "1.5.30"
     id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
 }
 
