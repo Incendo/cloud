35d1ec8548109cf6abb3534ba8108eeb1e3bb453
Jake Potrebic <jake.m.potrebic@gmail.com>
2021-10-02 10:21:38 -0700

    add license and missing javadoc comments

--- cloud-core/src/main/java/cloud/commandframework/arguments/standard/PseudoEnumArgument.java
+++ cloud-core/src/main/java/cloud/commandframework/arguments/standard/PseudoEnumArgument.java
@@ -1,3 +1,26 @@
+//
+// MIT License
+//
+// Copyright (c) 2021 Alexander Söderberg & Contributors
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
 package cloud.commandframework.arguments.standard;
 
 import cloud.commandframework.ArgumentDescription;
@@ -302,6 +325,12 @@ public class PseudoEnumArgument<C> extends CommandArgument<C, String> {
         private final Set<String> allowedValues;
         private final StringArgument.StringParser<C> stringParser;
 
+        /**
+         * Parser for pseudo-enums
+         *
+         * @param stringMode Mode to capture strings
+         * @param allowedValues Allowed values
+         */
         public PseudoEnumParser(
                 final StringArgument.@NonNull StringMode stringMode,
                 final @NonNull Set<String> allowedValues
@@ -320,7 +349,7 @@ public class PseudoEnumArgument<C> extends CommandArgument<C, String> {
                 return result;
             } else if (result.getParsedValue().isPresent()) {
                 String input = result.getParsedValue().get();
-                if (!allowedValues.contains(input)) {
+                if (!this.allowedValues.contains(input)) {
                     return ArgumentParseResult.failure(new PseudoEnumParseException(input, this.allowedValues, commandContext));
                 } else {
                     return result;
