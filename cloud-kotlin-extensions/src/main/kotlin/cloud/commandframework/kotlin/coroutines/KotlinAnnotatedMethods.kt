4f03f61b1fc84dee242b773ea50c721e9fde72e9
solonovamax <solonovamax@12oclockpoint.com>
2021-10-03 17:23:24 -0400

    Unwrap invocation target exceptions Unwrap invocation target exceptions when they occur in invoked kotlin suspend functions.

--- cloud-kotlin-extensions/src/main/kotlin/cloud/commandframework/kotlin/coroutines/KotlinAnnotatedMethods.kt
+++ cloud-kotlin-extensions/src/main/kotlin/cloud/commandframework/kotlin/coroutines/KotlinAnnotatedMethods.kt
@@ -27,13 +27,13 @@ import cloud.commandframework.annotations.AnnotationParser
 import cloud.commandframework.annotations.MethodCommandExecutionHandler
 import cloud.commandframework.context.CommandContext
 import cloud.commandframework.execution.CommandExecutionCoordinator
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.future.asCompletableFuture
+import java.lang.reflect.InvocationTargetException
 import java.lang.reflect.Method
 import java.util.concurrent.CompletableFuture
 import java.util.function.Predicate
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.future.future
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 import kotlin.reflect.full.callSuspend
@@ -45,15 +45,15 @@ import kotlin.reflect.jvm.kotlinFunction
  * @since 1.6.0
  */
 public fun <C> AnnotationParser<C>.installCoroutineSupport(
-    scope: CoroutineScope = GlobalScope,
-    context: CoroutineContext = EmptyCoroutineContext
+        scope: CoroutineScope = GlobalScope,
+        context: CoroutineContext = EmptyCoroutineContext
 ) {
     if (manager().commandExecutionCoordinator() is CommandExecutionCoordinator.SimpleCoordinator) {
         RuntimeException(
-            """You are highly advised to not use the simple command execution coordinator together
+                """You are highly advised to not use the simple command execution coordinator together
                             with coroutine support. Consider using the asynchronous command execution coordinator instead."""
         )
-            .printStackTrace()
+                .printStackTrace()
     }
 
     val predicate = Predicate<Method> { it.kotlinFunction?.isSuspend == true }
@@ -63,20 +63,23 @@ public fun <C> AnnotationParser<C>.installCoroutineSupport(
 }
 
 private class KotlinMethodCommandExecutionHandler<C>(
-    private val coroutineScope: CoroutineScope,
-    private val coroutineContext: CoroutineContext,
-    context: CommandMethodContext<C>
+        private val coroutineScope: CoroutineScope,
+        private val coroutineContext: CoroutineContext,
+        context: CommandMethodContext<C>
 ) : MethodCommandExecutionHandler<C>(context) {
 
     override fun executeFuture(commandContext: CommandContext<C>): CompletableFuture<Void?> {
         val instance = context().instance()
         val params = createParameterValues(commandContext, commandContext.flags(), false)
+
         // We need to propagate exceptions to the caller.
-        return coroutineScope
-            .async<Void?>(this@KotlinMethodCommandExecutionHandler.coroutineContext) {
-                context().method().kotlinFunction?.callSuspend(instance, *params.toTypedArray())
-                null
+        return coroutineScope.future(this@KotlinMethodCommandExecutionHandler.coroutineContext) {
+            try {
+                context().method().kotlinFunction!!.callSuspend(instance, *params.toTypedArray())
+            } catch (e: InvocationTargetException) { // unwrap invocation exception
+                e.cause?.let { throw it } ?: throw e // if cause exists, throw, else rethrow invocation exception
             }
-            .asCompletableFuture()
+            null
+        }
     }
 }
