2bf8d0f7091ba40715da839a6bdd5b79f14dfcc7
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2021-11-14 01:33:22 -0800

    fabric: Update loom

--- cloud-minecraft/cloud-fabric/build.gradle.kts
+++ cloud-minecraft/cloud-fabric/build.gradle.kts
@@ -2,26 +2,8 @@ import net.fabricmc.loom.task.AbstractRunTask
 import net.ltgt.gradle.errorprone.errorprone
 
 plugins {
-    id("quiet-fabric-loom") version "0.8-SNAPSHOT"
+    id("quiet-fabric-loom") version "0.10-SNAPSHOT"
 }
-
-/* set up a testmod source set */
-val testmod by sourceSets.creating {
-    val main = sourceSets.main.get()
-    compileClasspath += main.compileClasspath
-    runtimeClasspath += main.runtimeClasspath
-    dependencies.add(implementationConfigurationName, main.output)
-}
-
-val testmodJar by tasks.creating(Jar::class) {
-    archiveClassifier.set("testmod-dev")
-    group = LifecycleBasePlugin.BUILD_GROUP
-    from(testmod.output)
-}
-
-loom.unmappedModCollection.from(testmodJar)
-
-/* end of testmod setup */
 
 tasks {
     compileJava {
@@ -59,7 +41,7 @@ tasks {
 
 dependencies {
     minecraft("com.mojang", "minecraft", Versions.fabricMc)
-    mappings(minecraft.officialMojangMappings())
+    mappings(loom.officialMojangMappings())
     modImplementation("net.fabricmc", "fabric-loader", Versions.fabricLoader)
     modImplementation(fabricApi.module("fabric-command-api-v1", Versions.fabricApi))
     modImplementation(fabricApi.module("fabric-lifecycle-events-v1", Versions.fabricApi))
@@ -73,33 +55,21 @@ dependencies {
     api(include("io.leangen.geantyref", "geantyref", Versions.geantyref))
 }
 
-indra {
-    includeJavaSoftwareComponentInPublications(false)
-    configurePublications {
-        // add all the jars that should be included when publishing to maven
-        artifact(tasks.remapJar) {
-            builtBy(tasks.remapJar)
-        }
-        artifact(tasks.sourcesJar) {
-            builtBy(tasks.remapSourcesJar)
-        }
-        artifact(tasks.javadocJar) {
-            builtBy(tasks.javadocJar)
-        }
-
-        // Loom is broken with project dependencies in the same build (because it resolves dependencies during configuration)
-        // Please look away
-        pom {
-            withXml {
-                val dependencies = asNode().appendNode("dependencies")
-                sequenceOf("brigadier", "core", "services").forEach {
-                    val depNode = dependencies.appendNode("dependency")
-                    depNode.appendNode("groupId", project.group)
-                    depNode.appendNode("artifactId", "cloud-$it")
-                    depNode.appendNode("version", project.version)
-                    depNode.appendNode("scope", "compile")
-                }
-            }
-        }
-    }
+/* set up a testmod source set */
+val testmod: SourceSet by sourceSets.creating {
+    val main = sourceSets.main.get()
+    compileClasspath += main.compileClasspath
+    runtimeClasspath += main.runtimeClasspath
+    dependencies.add(implementationConfigurationName, main.output)
 }
+
+val testmodJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("testmod-dev")
+    group = LifecycleBasePlugin.BUILD_GROUP
+    from(testmod.output)
+}
+
+tasks.withType<AbstractRunTask> {
+    classpath(testmodJar)
+}
+/* end of testmod setup */
