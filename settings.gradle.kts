dd3531bef73c97f6206e1b72a2f447ed60a8b79f
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2021-11-14 01:33:22 -0800

    fabric: Update loom

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ pluginManagement {
     repositories {
         gradlePluginPortal()
         maven("https://maven.fabricmc.net")
+        maven("https://maven.quiltmc.org/repository/release/")
         maven("https://repo.jpenilla.xyz/snapshots/")
     }
     includeBuild("build-logic")
