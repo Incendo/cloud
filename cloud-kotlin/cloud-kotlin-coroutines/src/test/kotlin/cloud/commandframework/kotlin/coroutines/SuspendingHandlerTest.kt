653ab45659b6bba21d613474cf4eaec90f6911cc
Jason <11360596+jpenilla@users.noreply.github.com>
2021-11-15 16:35:21 -0800

    `cloud-kotlin-coroutines` (#318)
    * Builder coroutine support
    
    * Move coroutines to version catalog
    
    * Add kdocs
    
    * Add note about simple coordinator
    
    * Update changelog

---
+++ cloud-kotlin/cloud-kotlin-coroutines/src/test/kotlin/cloud/commandframework/kotlin/coroutines/SuspendingHandlerTest.kt
@@ -0,0 +1,75 @@
+//
+// MIT License
+//
+// Copyright (c) 2021 Alexander Söderberg & Contributors
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+package cloud.commandframework.kotlin.coroutines
+
+import cloud.commandframework.CommandManager
+import cloud.commandframework.execution.AsynchronousCommandExecutionCoordinator
+import cloud.commandframework.internal.CommandRegistrationHandler
+import cloud.commandframework.kotlin.coroutines.extension.suspendingHandler
+import cloud.commandframework.kotlin.extension.buildAndRegister
+import cloud.commandframework.meta.CommandMeta
+import cloud.commandframework.meta.SimpleCommandMeta
+import kotlinx.coroutines.future.await
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Test
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class SuspendingHandlerTest {
+
+    companion object {
+        val executorService: ExecutorService = Executors.newSingleThreadExecutor()
+    }
+
+    @Test
+    fun test(): Unit = runBlocking {
+        val manager = TestCommandManager()
+
+        manager.buildAndRegister("suspend") {
+            suspendingHandler {
+                println("called from thread: ${Thread.currentThread().name}")
+
+                someSuspendingFunction()
+            }
+        }
+
+        manager.executeCommand(TestCommandSender(), "suspend").await()
+    }
+
+    suspend fun someSuspendingFunction() {}
+
+    private class TestCommandSender
+
+    private class TestCommandManager : CommandManager<TestCommandSender>(
+        AsynchronousCommandExecutionCoordinator.newBuilder<TestCommandSender>()
+            .withExecutor(executorService)
+            .build(),
+        CommandRegistrationHandler.nullCommandRegistrationHandler()
+    ) {
+
+        override fun hasPermission(sender: TestCommandSender, permission: String): Boolean = true
+
+        override fun createDefaultCommandMeta(): CommandMeta = SimpleCommandMeta.empty()
+    }
+}
