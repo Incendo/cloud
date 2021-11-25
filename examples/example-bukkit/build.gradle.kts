4c644257f8959d51640b388d2460d267b39b41c6
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2021-11-25 14:23:19 -0800

    Update CraftBukkit reflection for 1.18

--- examples/example-bukkit/build.gradle.kts
+++ examples/example-bukkit/build.gradle.kts
@@ -33,15 +33,14 @@ tasks {
         minecraftVersion("1.17.1")
         runDirectory(file("run/latest"))
         javaLauncher.set(project.javaToolchains.launcherFor {
-            languageVersion.set(JavaLanguageVersion.of(16))
+            languageVersion.set(JavaLanguageVersion.of(17))
         })
     }
 
     // Setup a run task for each supported version
     mapOf(
-            setOf("1.8.8", "1.9.4", "1.10.2", "1.11.2", "1.12.2") to 8,
-            setOf("1.13.2", "1.14.4", "1.15.2") to 11,
-            setOf("1.16.5", "1.17.1") to 16
+            setOf("1.8.8", "1.9.4", "1.10.2", "1.11.2") to 11,
+            setOf("1.12.2", "1.13.2", "1.14.4", "1.15.2", "1.16.5", "1.17.1") to 17,
     ).forEach { (minecraftVersions, javaVersion) ->
         for (version in minecraftVersions) {
             createVersionedRun(version, javaVersion)
@@ -57,6 +56,7 @@ fun TaskContainerScope.createVersionedRun(
     pluginJars.from(shadowJar.flatMap { it.archiveFile })
     minecraftVersion(version)
     runDirectory(file("run/$version"))
+    systemProperty("Paper.IgnoreJavaVersion", true)
     javaLauncher.set(project.javaToolchains.launcherFor {
         languageVersion.set(JavaLanguageVersion.of(javaVersion))
     })
