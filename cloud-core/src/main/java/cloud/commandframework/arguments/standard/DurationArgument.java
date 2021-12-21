a36f03e7d386e61e4c5275a7799fe92d31bba618
Tadhg Boyle <tadhgsmboyle@gmail.com>
2021-12-21 12:26:30 -0800

    add DurationArgument

---
+++ cloud-core/src/main/java/cloud/commandframework/arguments/standard/DurationArgument.java
@@ -0,0 +1,321 @@
+//
+// MIT License
+//
+// Copyright (c) 2021 Alexander Söderberg & Contributors
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+package cloud.commandframework.arguments.standard;
+
+import cloud.commandframework.ArgumentDescription;
+import cloud.commandframework.arguments.CommandArgument;
+import cloud.commandframework.arguments.parser.ArgumentParseResult;
+import cloud.commandframework.arguments.parser.ArgumentParser;
+import cloud.commandframework.captions.CaptionVariable;
+import cloud.commandframework.captions.StandardCaptionKeys;
+import cloud.commandframework.context.CommandContext;
+import cloud.commandframework.exceptions.parsing.NoInputProvidedException;
+import cloud.commandframework.exceptions.parsing.ParserException;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.Queue;
+import java.util.function.BiFunction;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+
+@SuppressWarnings("unused")
+public final class DurationArgument<C> extends CommandArgument<C, Duration> {
+
+    private DurationArgument(
+            final boolean required,
+            final @NonNull String name,
+            final @NonNull String defaultValue,
+            final @Nullable BiFunction<@NonNull CommandContext<C>, @NonNull String,
+                    @NonNull List<@NonNull String>> suggestionsProvider,
+            final @NonNull ArgumentDescription defaultDescription
+    ) {
+        super(
+                required,
+                name,
+                new DurationArgument.DurationParser<>(),
+                defaultValue,
+                Duration.class,
+                suggestionsProvider,
+                defaultDescription
+        );
+    }
+
+    /**
+     * Create a new builder
+     *
+     * @param name Name of the component
+     * @param <C>  Command sender type
+     * @return Created builder
+     */
+    public static <C> @NonNull Builder<C> newBuilder(final @NonNull String name) {
+        return new Builder<>(name);
+    }
+
+    /**
+     * Create a new required command component
+     *
+     * @param name Component name
+     * @param <C>  Command sender type
+     * @return Created component
+     */
+    public static <C> @NonNull CommandArgument<C, Duration> of(final @NonNull String name) {
+        return DurationArgument.<C>newBuilder(name).asRequired().build();
+    }
+
+    /**
+     * Create a new optional command component
+     *
+     * @param name Component name
+     * @param <C>  Command sender type
+     * @return Created component
+     */
+    public static <C> @NonNull CommandArgument<C, Duration> optional(final @NonNull String name) {
+        return DurationArgument.<C>newBuilder(name).asOptional().build();
+    }
+
+    /**
+     * Create a new required command component with a default value
+     *
+     * @param name            Component name
+     * @param defaultDuration Default duration
+     * @param <C>             Command sender type
+     * @return Created component
+     */
+    public static <C> @NonNull CommandArgument<C, Duration> optional(
+            final @NonNull String name,
+            final @NonNull String defaultDuration
+    ) {
+        return DurationArgument.<C>newBuilder(name).asOptionalWithDefault(defaultDuration).build();
+    }
+
+
+    public static final class Builder<C> extends CommandArgument.Builder<C, Duration> {
+
+        private Builder(final @NonNull String name) {
+            super(Duration.class, name);
+        }
+
+        /**
+         * Builder a new boolean component
+         *
+         * @return Constructed component
+         */
+        @Override
+        public @NonNull DurationArgument<C> build() {
+            return new DurationArgument<>(this.isRequired(), this.getName(), this.getDefaultValue(),
+                    this.getSuggestionsProvider(), this.getDefaultDescription()
+            );
+        }
+
+    }
+
+
+    public static final class DurationParser<C> implements ArgumentParser<C, Duration> {
+
+        @Override
+        public @NonNull ArgumentParseResult<Duration> parse(
+                final @NonNull CommandContext<C> commandContext,
+                final @NonNull Queue<String> inputQueue
+        ) {
+            final String input = inputQueue.peek();
+            if (input == null) {
+                return ArgumentParseResult.failure(new NoInputProvidedException(
+                        DurationArgument.DurationParseException.class,
+                        commandContext
+                ));
+            }
+            inputQueue.remove();
+
+            /*
+             * Matches durations in the format of: "2d15h7m12s"
+             */
+            final Matcher matcher = Pattern.compile("(([1-9][0-9]+|[1-9])[dhms])").matcher(input);
+
+            Duration duration = Duration.ofNanos(0);
+
+            while (matcher.find()) {
+                String group = matcher.group();
+                String timeUnit = String.valueOf(group.charAt(group.length() - 1));
+                int timeValue = Integer.parseInt(group.substring(0, group.length() - 1));
+                switch (timeUnit) {
+                    case "d":
+                        duration = duration.plusDays(timeValue);
+                        break;
+                    case "h":
+                        duration = duration.plusHours(timeValue);
+                        break;
+                    case "m":
+                        duration = duration.plusMinutes(timeValue);
+                        break;
+                    case "s":
+                        duration = duration.plusSeconds(timeValue);
+                        break;
+                    default:
+                        return ArgumentParseResult.failure(new DurationArgument.DurationParseException(input, commandContext));
+                }
+            }
+
+            if (duration.isZero()) {
+                return ArgumentParseResult.failure(new DurationArgument.DurationParseException(input, commandContext));
+            }
+
+            return ArgumentParseResult.success(duration);
+        }
+
+        @Override
+        public @NonNull List<@NonNull String> suggestions(
+                final @NonNull CommandContext<C> commandContext,
+                final @NonNull String input
+        ) {
+            char[] chars = input.toLowerCase(Locale.ROOT).toCharArray();
+
+            Stream<String> nums = IntStream.range(1, 10).boxed()
+                    .sorted()
+                    .map(String::valueOf);
+
+            if (chars.length == 0) {
+                return nums.collect(Collectors.toList());
+            }
+
+            List<Character> units = Arrays.asList('d', 'h', 'm', 's');
+
+            char last = chars[chars.length - 1];
+
+            // 1d_, 5d4m_, etc
+            if (Character.isLetter(last)) {
+
+                // if the input contains all the units already, then we can't suggest anything
+                int found = 0;
+                for (char unit : units) {
+                    if (input.contains(String.valueOf(unit))) {
+                        found++;
+                    }
+                }
+                if (found == units.size()) {
+                    return new ArrayList<>();
+                }
+
+                // if the input is just a letter ("d"), return nothing
+                if (!units.contains(last) || chars.length == 1) {
+                    return new ArrayList<>();
+                }
+
+                // if the input is just a letter repeated ("dd"), return nothing
+                char secondLast = chars[chars.length - 2];
+                if (last == secondLast || Character.isLetter(secondLast)) {
+                    return new ArrayList<>();
+                }
+
+                // if the input is a letter which is already in the input, return nothing ("1d2d")
+                if (charsContainsMultiple(chars, last)) {
+                    return new ArrayList<>();
+                }
+
+                // add each of the numbers to the input
+                nums = nums.map(i -> input + i);
+                List<String> numList = nums.collect(Collectors.toList());
+
+                List<String> completions = new ArrayList<>();
+                // add each of the time units to each of the possible combos
+                for (String num : numList) {
+                    for (char unit : units) {
+                        if (unit == last) {
+                            continue;
+                        }
+                        if (!charsContainsMultiple(chars, unit)) {
+                            completions.add(num + unit);
+                        }
+                    }
+                }
+                return completions;
+            }
+
+            // 1d5_, 5d4m2_, etc
+            return units.stream()
+                    .filter(c -> !input.contains(String.valueOf(c)))
+                    .map(c -> input + c)
+                    .collect(Collectors.toList());
+        }
+
+    }
+
+    private static boolean charsContainsMultiple(final char[] chars, final char c) {
+        int count = 0;
+        for (char ch : chars) {
+            if (ch == c) {
+                count++;
+            }
+        }
+        return count > 1;
+    }
+
+
+    /**
+     * Duration parse exception
+     */
+    public static final class DurationParseException extends ParserException {
+
+        private static final long serialVersionUID = 7632293268451349508L;
+        private final String input;
+
+        /**
+         * Construct a new Duration parse exception
+         *
+         * @param input   String input
+         * @param context Command context
+         */
+        public DurationParseException(
+                final @NonNull String input,
+                final @NonNull CommandContext<?> context
+        ) {
+            super(
+                    DurationArgument.DurationParser.class,
+                    context,
+                    StandardCaptionKeys.ARGUMENT_PARSE_FAILURE_DURATION,
+                    CaptionVariable.of("input", input)
+            );
+            this.input = input;
+        }
+
+        /**
+         * Get the supplied input
+         *
+         * @return String value
+         */
+        public @NonNull String getInput() {
+            return this.input;
+        }
+
+    }
+
+}
