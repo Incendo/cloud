05e91424a2095255f30f5ac4649e499f5fbf21bb
Citymonstret <alexander.soderberg@incendo.org>
2022-05-28 13:49:33 +0200

    feat(core): add EnumParser unit tests

--- cloud-core/src/main/java/cloud/commandframework/arguments/standard/EnumArgument.java
+++ cloud-core/src/main/java/cloud/commandframework/arguments/standard/EnumArgument.java
@@ -34,6 +34,7 @@ import cloud.commandframework.exceptions.parsing.NoInputProvidedException;
 import cloud.commandframework.exceptions.parsing.ParserException;
 import java.util.EnumSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Queue;
 import java.util.function.BiFunction;
 import java.util.stream.Collectors;
@@ -254,6 +255,23 @@ public class EnumArgument<C, E extends Enum<E>> extends CommandArgument<C, E> {
          */
         public @NonNull Class<? extends Enum<?>> getEnumClass() {
             return this.enumClass;
+        }
+
+        @Override
+        public boolean equals(final Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || this.getClass() != o.getClass()) {
+                return false;
+            }
+            final EnumParseException that = (EnumParseException) o;
+            return this.input.equals(that.input) && this.enumClass.equals(that.enumClass);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(this.input, this.enumClass);
         }
 
     }
