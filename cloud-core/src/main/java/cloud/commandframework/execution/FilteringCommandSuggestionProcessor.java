12c140f137b9ef00608d39f73d9ff8f27e72ad63
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2022-11-26 12:42:09 -0700

    Add handling for spaces to FilteringCommandSuggestionProcessor

--- cloud-core/src/main/java/cloud/commandframework/execution/FilteringCommandSuggestionProcessor.java
+++ cloud-core/src/main/java/cloud/commandframework/execution/FilteringCommandSuggestionProcessor.java
@@ -27,8 +27,10 @@ import cloud.commandframework.execution.preprocessor.CommandPreprocessingContext
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.BiPredicate;
 import org.apiguardian.api.API;
 import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
 
 /**
  * Command suggestion processor filters suggestions based on the remaining unconsumed input in the
@@ -47,7 +49,7 @@ public final class FilteringCommandSuggestionProcessor<C> implements CommandSugg
      */
     @API(status = API.Status.STABLE)
     public FilteringCommandSuggestionProcessor() {
-        this(startsWith(false));
+        this(Filter.startsWith(false));
     }
 
     /**
@@ -74,49 +76,16 @@ public final class FilteringCommandSuggestionProcessor<C> implements CommandSugg
         }
         final List<String> suggestions = new ArrayList<>(strings.size());
         for (final String suggestion : strings) {
-            if (this.filter.test(context, suggestion, input)) {
-                suggestions.add(suggestion);
+            final @Nullable String filtered = this.filter.filter(context, suggestion, input);
+            if (filtered != null) {
+                suggestions.add(filtered);
             }
         }
         return suggestions;
     }
 
     /**
-     * Create a filter using {@link String#startsWith(String)} that can optionally ignore case.
-     *
-     * @param ignoreCase whether to ignore case
-     * @param <C>        sender type
-     * @return new filter
-     * @since 1.8.0
-     */
-    @API(status = API.Status.STABLE, since = "1.8.0")
-    public static <C> Filter<C> startsWith(final boolean ignoreCase) {
-        if (ignoreCase) {
-            return (ctx, suggestion, input) -> suggestion
-                    .toLowerCase(Locale.ROOT)
-                    .startsWith(input.toLowerCase(Locale.ROOT));
-        }
-        return (ctx, suggestion, input) -> suggestion.startsWith(input);
-    }
-
-    /**
-     * Create a filter using {@link String#contains(CharSequence)} that can optionally ignore case.
-     *
-     * @param ignoreCase whether to ignore case
-     * @param <C>        sender type
-     * @return new filter
-     * @since 1.8.0
-     */
-    @API(status = API.Status.STABLE, since = "1.8.0")
-    public static <C> Filter<C> contains(final boolean ignoreCase) {
-        if (ignoreCase) {
-            return (ctx, suggestion, input) -> suggestion.toLowerCase(Locale.ROOT).contains(input.toLowerCase(Locale.ROOT));
-        }
-        return (ctx, suggestion, input) -> suggestion.contains(input);
-    }
-
-    /**
-     * Filter function that tests each suggestion against the input and context.
+     * Filter function that tests (and potentially changes) each suggestion against the input and context.
      *
      * @param <C> sender type
      * @since 1.8.0
@@ -126,15 +95,165 @@ public final class FilteringCommandSuggestionProcessor<C> implements CommandSugg
     public interface Filter<C> {
 
         /**
-         * Tests a potential suggestion against the input and context.
+         * Filters a potential suggestion against the input and context.
          *
          * @param context    context
          * @param suggestion potential suggestion
          * @param input      remaining unconsumed input
-         * @return whether to accept the suggestion
+         * @return possibly modified suggestion or null to deny
          * @since 1.8.0
          */
         @API(status = API.Status.STABLE, since = "1.8.0")
-        boolean test(CommandPreprocessingContext<C> context, String suggestion, String input);
+        @Nullable String filter(
+                @NonNull CommandPreprocessingContext<C> context,
+                @NonNull String suggestion,
+                @NonNull String input
+        );
+
+        /**
+         * Returns a new {@link Filter} which tests this filter, and if the result
+         * is non-null, then filters with {@code and}.
+         *
+         * @param and next filter
+         * @return combined filter
+         * @since 1.8.0
+         */
+        @API(status = API.Status.STABLE, since = "1.8.0")
+        default @NonNull Filter<C> and(final @NonNull Filter<C> and) {
+            return (ctx, suggestion, input) -> {
+                final @Nullable String filtered = this.filter(ctx, suggestion, input);
+                if (filtered == null) {
+                    return null;
+                }
+                return and.filter(ctx, filtered, input);
+            };
+        }
+
+        /**
+         * Returns a new {@link Filter} that tests this filter, and then
+         * uses {@link #trimBeforeLastSpace()} if the result is non-null.
+         *
+         * @return combined filter
+         * @since 1.8.0
+         */
+        @API(status = API.Status.STABLE, since = "1.8.0")
+        default Filter<C> andTrimBeforeLastSpace() {
+            return this.and(trimBeforeLastSpace());
+        }
+
+        /**
+         * Create a filter using {@link String#startsWith(String)} that can optionally ignore case.
+         *
+         * @param ignoreCase whether to ignore case
+         * @param <C>        sender type
+         * @return new filter
+         * @since 1.8.0
+         */
+        @API(status = API.Status.STABLE, since = "1.8.0")
+        static <C> @NonNull Filter<C> startsWith(final boolean ignoreCase) {
+            final BiPredicate<String, String> test = ignoreCase
+                    ? (suggestion, input) -> suggestion.toLowerCase(Locale.ROOT).startsWith(input.toLowerCase(Locale.ROOT))
+                    : String::startsWith;
+            return Simple.contextFree(test);
+        }
+
+        /**
+         * Create a filter using {@link String#contains(CharSequence)} that can optionally ignore case.
+         *
+         * @param ignoreCase whether to ignore case
+         * @param <C>        sender type
+         * @return new filter
+         * @since 1.8.0
+         */
+        @API(status = API.Status.STABLE, since = "1.8.0")
+        static <C> @NonNull Filter<C> contains(final boolean ignoreCase) {
+            final BiPredicate<String, String> test = ignoreCase
+                    ? (suggestion, input) -> suggestion.toLowerCase(Locale.ROOT).contains(input.toLowerCase(Locale.ROOT))
+                    : String::contains;
+            return Simple.contextFree(test);
+        }
+
+        /**
+         * Create a filter which does extra processing when the input contains spaces.
+         *
+         * <p>Will return the portion of the suggestion which is after the last space in
+         * the input.</p>
+         *
+         * @param <C> sender type
+         * @return new filter
+         * @since 1.8.0
+         */
+        @API(status = API.Status.STABLE, since = "1.8.0")
+        static <C> @NonNull Filter<C> trimBeforeLastSpace() {
+            return (context, suggestion, input) -> {
+                final int lastSpace = input.lastIndexOf(' ');
+                // No spaces in input, don't do anything
+                if (lastSpace == -1) {
+                    return suggestion;
+                }
+
+                // Always use case-insensitive here
+                if (suggestion.toLowerCase(Locale.ROOT).startsWith(input.toLowerCase(Locale.ROOT))) {
+                    return suggestion.substring(lastSpace + 1);
+                }
+
+                return null;
+            };
+        }
+
+        /**
+         * Simple version of {@link Filter} which doesn't modify suggestions.
+         *
+         * <p>Returns boolean instead of nullable String.</p>
+         *
+         * @param <C> sender type
+         * @since 1.8.0
+         */
+        @API(status = API.Status.STABLE, since = "1.8.0")
+        @FunctionalInterface
+        interface Simple<C> extends Filter<C> {
+
+            /**
+             * Tests a suggestion against the context and input.
+             *
+             * @param context    context
+             * @param suggestion potential suggestion
+             * @param input      remaining unconsumed input
+             * @return whether to accept the suggestion
+             * @since 1.8.0
+             */
+            @API(status = API.Status.STABLE, since = "1.8.0")
+            boolean test(
+                    @NonNull CommandPreprocessingContext<C> context,
+                    @NonNull String suggestion,
+                    @NonNull String input
+            );
+
+            @Override
+            @SuppressWarnings("FunctionalInterfaceMethodChanged")
+            default @Nullable String filter(
+                    @NonNull CommandPreprocessingContext<C> context,
+                    @NonNull String suggestion,
+                    @NonNull String input
+            ) {
+                if (this.test(context, suggestion, input)) {
+                    return suggestion;
+                }
+                return null;
+            }
+
+            /**
+             * Create a new context-free {@link Simple}.
+             *
+             * @param test predicate
+             * @param <C>  sender type
+             * @return simple filter
+             * @since 1.8.0
+             */
+            @API(status = API.Status.STABLE, since = "1.8.0")
+            static <C> @NonNull Simple<C> contextFree(final @NonNull BiPredicate<String, String> test) {
+                return (ctx, suggestion, input) -> test.test(suggestion, input);
+            }
+        }
     }
 }
