4e02686a1f45447cbb4fa7c1e608336ebfa491a0
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-02-21 20:50:42 -0700

    Update loom to 1.1 & move fabric-permissions-api to modApi to match pre-loom 1.1 behavior of it being on the compile classpath for consumers

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,7 +21,7 @@ pluginManagement {
 
 plugins {
     id("ca.stellardrift.polyglot-version-catalogs") version "6.0.1"
-    id("quiet-fabric-loom") version "1.0-SNAPSHOT"
+    id("quiet-fabric-loom") version "1.1-SNAPSHOT"
 }
 
 dependencyResolutionManagement {
