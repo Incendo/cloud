ba4f1244e5035b04b4816d0d2fe374182b310d19
Alexander Söderberg <alexander.soderberg@noxcrew.com>
2023-11-15 15:22:43 +0100

    add tests for default values

---
+++ cloud-core/src/test/java/cloud/commandframework/feature/DefaultValueTest.java
@@ -0,0 +1,93 @@
+//
+// MIT License
+//
+// Copyright (c) 2022 Alexander Söderberg & Contributors
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+package cloud.commandframework.feature;
+
+import cloud.commandframework.CommandManager;
+import cloud.commandframework.TestCommandSender;
+import cloud.commandframework.arguments.CommandArgument;
+import cloud.commandframework.arguments.DefaultValue;
+import cloud.commandframework.arguments.standard.IntegerArgument;
+import cloud.commandframework.execution.CommandResult;
+import java.util.concurrent.ThreadLocalRandom;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static cloud.commandframework.util.TestUtils.createManager;
+import static com.google.common.truth.Truth.assertThat;
+
+class DefaultValueTest {
+
+    private CommandManager<TestCommandSender> commandManager;
+
+    @BeforeEach
+    void setup() {
+        this.commandManager = createManager();
+    }
+
+    @Test
+    void Constant_HappyFlow_Success() throws Exception {
+        // Arrange
+        final CommandArgument<TestCommandSender, Integer> argument = IntegerArgument.of("int");
+        this.commandManager.command(
+                this.commandManager.commandBuilder("test").optional(argument, DefaultValue.constant(5))
+        );
+
+        // Act
+        final CommandResult<TestCommandSender> result = this.commandManager.executeCommand(new TestCommandSender(), "test").get();
+
+        // Assert
+        assertThat(result.getCommandContext().get(argument)).isEqualTo(5);
+    }
+
+    @Test
+    void Dynamic_HappyFlow_Success() throws Exception {
+        // Arrange
+        final CommandArgument<TestCommandSender, Integer> argument = IntegerArgument.of("int");
+        final DefaultValue<TestCommandSender, Integer> defaultValue = ctx -> ThreadLocalRandom.current().nextInt();
+        this.commandManager.command(
+                this.commandManager.commandBuilder("test").optional(argument, defaultValue)
+        );
+
+        // Act
+        final CommandResult<TestCommandSender> result = this.commandManager.executeCommand(new TestCommandSender(), "test").get();
+
+        // Assert
+        assertThat(result.getCommandContext().get(argument)).isNotNull();
+    }
+
+    @Test
+    void Parsed_HappyFlow_Success() throws Exception {
+        // Arrange
+        final CommandArgument<TestCommandSender, Integer> argument = IntegerArgument.of("int");
+        this.commandManager.command(
+                this.commandManager.commandBuilder("test").optional(argument, DefaultValue.parsed("5"))
+        );
+
+        // Act
+        final CommandResult<TestCommandSender> result = this.commandManager.executeCommand(new TestCommandSender(), "test").get();
+
+        // Assert
+        assertThat(result.getCommandContext().get(argument)).isEqualTo(5);
+    }
+}
