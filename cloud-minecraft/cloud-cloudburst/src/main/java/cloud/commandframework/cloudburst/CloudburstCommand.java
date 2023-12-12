1064544b87aa3817156b2448515c869cfc29cf2c
Alexander Söderberg <alexander.soderberg@incendo.org>
2023-12-12 12:14:07 +0100

    cloudburst

--- cloud-minecraft/cloud-cloudburst/src/main/java/cloud/commandframework/cloudburst/CloudburstCommand.java
+++ cloud-minecraft/cloud-cloudburst/src/main/java/cloud/commandframework/cloudburst/CloudburstCommand.java
@@ -25,14 +25,7 @@ package cloud.commandframework.cloudburst;
 
 import cloud.commandframework.Command;
 import cloud.commandframework.CommandComponent;
-import cloud.commandframework.exceptions.ArgumentParseException;
-import cloud.commandframework.exceptions.CommandExecutionException;
-import cloud.commandframework.exceptions.InvalidCommandSenderException;
-import cloud.commandframework.exceptions.InvalidSyntaxException;
-import cloud.commandframework.exceptions.NoPermissionException;
-import cloud.commandframework.exceptions.NoSuchCommandException;
 import java.util.Collection;
-import java.util.concurrent.CompletionException;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.cloudburstmc.server.command.CommandSender;
 import org.cloudburstmc.server.command.PluginCommand;
@@ -40,12 +33,6 @@ import org.cloudburstmc.server.command.data.CommandData;
 import org.cloudburstmc.server.plugin.Plugin;
 
 final class CloudburstCommand<C> extends PluginCommand<Plugin> {
-
-    private static final String MESSAGE_INTERNAL_ERROR = "An internal error occurred while attempting to perform this command.";
-    private static final String MESSAGE_NO_PERMS =
-            "I'm sorry, but you do not have permission to perform this command. "
-                    + "Please contact the server administrators if you believe that this is in error.";
-    private static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command. Type \"/help\" for help.";
 
     private final CommandComponent<C> command;
     private final CloudburstCommandManager<C> manager;
@@ -78,73 +65,7 @@ final class CloudburstCommand<C> extends PluginCommand<Plugin> {
             builder.append(" ").append(string);
         }
         final C sender = this.manager.getCommandSenderMapper().apply(commandSender);
-        this.manager.executeCommand(
-                        sender,
-                        builder.toString()
-                )
-                .whenComplete((commandResult, throwable) -> {
-                    if (throwable != null) {
-                        if (throwable instanceof CompletionException) {
-                            throwable = throwable.getCause();
-                        }
-                        final Throwable finalThrowable = throwable;
-                        if (throwable instanceof InvalidSyntaxException) {
-                            this.manager.handleException(sender,
-                                    InvalidSyntaxException.class,
-                                    (InvalidSyntaxException) throwable, (c, e) ->
-                                            commandSender.sendMessage(
-                                                    "Invalid Command Syntax. "
-                                                            + "Correct command syntax is: "
-                                                            + "/"
-                                                            + ((InvalidSyntaxException) finalThrowable)
-                                                            .getCorrectSyntax())
-                            );
-                        } else if (throwable instanceof InvalidCommandSenderException) {
-                            this.manager.handleException(sender,
-                                    InvalidCommandSenderException.class,
-                                    (InvalidCommandSenderException) throwable, (c, e) ->
-                                            commandSender.sendMessage(finalThrowable.getMessage())
-                            );
-                        } else if (throwable instanceof NoPermissionException) {
-                            this.manager.handleException(sender,
-                                    NoPermissionException.class,
-                                    (NoPermissionException) throwable, (c, e) ->
-                                            commandSender.sendMessage(MESSAGE_NO_PERMS)
-                            );
-                        } else if (throwable instanceof NoSuchCommandException) {
-                            this.manager.handleException(sender,
-                                    NoSuchCommandException.class,
-                                    (NoSuchCommandException) throwable, (c, e) ->
-                                            commandSender.sendMessage(MESSAGE_UNKNOWN_COMMAND)
-                            );
-                        } else if (throwable instanceof ArgumentParseException) {
-                            this.manager.handleException(sender,
-                                    ArgumentParseException.class,
-                                    (ArgumentParseException) throwable, (c, e) ->
-                                            commandSender.sendMessage(
-                                                    "Invalid Command Argument: "
-                                                            + finalThrowable.getCause().getMessage())
-                            );
-                        } else if (throwable instanceof CommandExecutionException) {
-                            this.manager.handleException(sender,
-                                    CommandExecutionException.class,
-                                    (CommandExecutionException) throwable, (c, e) -> {
-                                        commandSender.sendMessage(MESSAGE_INTERNAL_ERROR);
-                                        this.manager.getOwningPlugin().getLogger().error(
-                                                "Exception executing command handler",
-                                                finalThrowable.getCause()
-                                        );
-                                    }
-                            );
-                        } else {
-                            commandSender.sendMessage(MESSAGE_INTERNAL_ERROR);
-                            this.manager.getOwningPlugin().getLogger().error(
-                                    "An unhandled exception was thrown during command execution",
-                                    throwable
-                            );
-                        }
-                    }
-                });
+        this.manager.executeCommand(sender, builder.toString());
         return true;
     }
 }
