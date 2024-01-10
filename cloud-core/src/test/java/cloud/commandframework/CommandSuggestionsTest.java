c2b3c8abfdb27824c177653f1a7c3937846fdeb1
Alexander Söderberg <4096670+Citymonstret@users.noreply.github.com>
2024-01-10 22:31:44 +0100

    fix(core): always suggest unused flag names when suggesting flags (#623)
    fix(core): suggest flag name when suggesting aliases

--- cloud-core/src/test/java/cloud/commandframework/CommandSuggestionsTest.java
+++ cloud-core/src/test/java/cloud/commandframework/CommandSuggestionsTest.java
@@ -343,7 +343,7 @@ class CommandSuggestionsTest {
         final List<? extends Suggestion> suggestions = this.manager.suggestionFactory().suggestImmediately(new TestCommandSender(), input).list();
 
         // Assert
-        Assertions.assertEquals(suggestionList("-fs", "-ft", "-f"), suggestions);
+        Assertions.assertEquals(suggestionList("--first", "-fs", "-ft", "-f"), suggestions);
     }
 
     @Test
@@ -362,7 +362,7 @@ class CommandSuggestionsTest {
         final List<? extends Suggestion> suggestions = this.manager.suggestionFactory().suggestImmediately(new TestCommandSender(), input).list();
 
         // Assert
-        Assertions.assertEquals(suggestionList("-st", "-s"), suggestions);
+        Assertions.assertEquals(suggestionList("--second", "-st", "-s"), suggestions);
     }
 
     @Test
@@ -631,7 +631,7 @@ class CommandSuggestionsTest {
         assertThat(suggestions2).containsExactlyElementsIn(suggestionList("hello"));
         assertThat(suggestions3).containsExactlyElementsIn(suggestionList("--flag", "--flag2"));
         assertThat(suggestions4).containsExactlyElementsIn(suggestionList("--flag", "--flag2"));
-        assertThat(suggestions5).containsExactlyElementsIn(suggestionList("--flag2", "-f"));
+        assertThat(suggestions5).containsExactlyElementsIn(suggestionList("--flag2", "--flag", "-f"));
         assertThat(suggestions6).isEmpty();
     }
 
@@ -659,7 +659,7 @@ class CommandSuggestionsTest {
         assertThat(suggestions2).isEmpty();
         assertThat(suggestions3).containsExactlyElementsIn(suggestionList("--flag", "--flag2"));
         assertThat(suggestions4).containsExactlyElementsIn(suggestionList("--flag", "--flag2"));
-        assertThat(suggestions5).containsExactlyElementsIn(suggestionList("--flag2", "-f"));
+        assertThat(suggestions5).containsExactlyElementsIn(suggestionList("--flag2", "--flag", "-f"));
         assertThat(suggestions6).isEmpty();
     }
 
@@ -726,7 +726,7 @@ class CommandSuggestionsTest {
         assertThat(suggestions2).containsExactlyElementsIn(suggestionList("hello"));
         assertThat(suggestions3).containsExactlyElementsIn(suggestionList("--flag", "--flag2"));
         assertThat(suggestions4).containsExactlyElementsIn(suggestionList("--flag", "--flag2"));
-        assertThat(suggestions5).containsExactlyElementsIn(suggestionList("--flag2", "-f"));
+        assertThat(suggestions5).containsExactlyElementsIn(suggestionList("--flag2", "--flag", "-f"));
         assertThat(suggestions6).isEmpty();
     }
 
@@ -755,7 +755,7 @@ class CommandSuggestionsTest {
         assertThat(suggestions2).isEmpty();
         assertThat(suggestions3).containsExactlyElementsIn(suggestionList("--flag", "--flag2"));
         assertThat(suggestions4).containsExactlyElementsIn(suggestionList("--flag", "--flag2"));
-        assertThat(suggestions5).containsExactlyElementsIn(suggestionList("--flag2", "-f"));
+        assertThat(suggestions5).containsExactlyElementsIn(suggestionList("--flag2", "--flag", "-f"));
         assertThat(suggestions6).isEmpty();
     }
 
@@ -784,7 +784,7 @@ class CommandSuggestionsTest {
                 arguments("command --", suggestionList("--flag", "--flog")),
                 arguments("command --f", suggestionList("--flag", "--flog")),
                 arguments("command --fla", suggestionList("--flag")),
-                arguments("command -f", suggestionList("--flog", "-f")),
+                arguments("command -f", suggestionList("--flag", "--flog", "-f")),
                 arguments("command -", suggestionList("--flag", "--flog", "-f")),
                 arguments("command -f ", suggestionList("foo", "bar")),
                 arguments("command -f b", suggestionList("bar"))
