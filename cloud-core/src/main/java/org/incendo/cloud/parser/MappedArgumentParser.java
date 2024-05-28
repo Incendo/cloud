b8eac716eddc5c014cee5c73c13e293dc0ac6ed8
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-05-28 10:21:20 -0700

    feat(core): Extract interface from MappedArgumentParser

--- cloud-core/src/main/java/org/incendo/cloud/parser/MappedArgumentParser.java
+++ cloud-core/src/main/java/org/incendo/cloud/parser/MappedArgumentParser.java
@@ -26,12 +26,7 @@ package org.incendo.cloud.parser;
 import java.util.concurrent.CompletableFuture;
 import org.apiguardian.api.API;
 import org.checkerframework.checker.nullness.qual.NonNull;
-import org.checkerframework.checker.nullness.qual.Nullable;
 import org.incendo.cloud.context.CommandContext;
-import org.incendo.cloud.context.CommandInput;
-import org.incendo.cloud.suggestion.SuggestionProvider;
-
-import static java.util.Objects.requireNonNull;
 
 /**
  * An argument parser which wraps another argument parser, converting the output type.
@@ -41,81 +36,17 @@ import static java.util.Objects.requireNonNull;
  * @param <O> mapped output type
  */
 @API(status = API.Status.STABLE)
-public final class MappedArgumentParser<C, I, O> implements ArgumentParser.FutureArgumentParser<C, O> {
-
-    private final ArgumentParser<C, I> base;
-    private final Mapper<C, I, O> mapper;
-
-    MappedArgumentParser(
-            final ArgumentParser<C, I> base,
-            final Mapper<C, I, O> mapper
-    ) {
-        this.base = base;
-        this.mapper = mapper;
-    }
+public interface MappedArgumentParser<C, I, O> extends ArgumentParser<C, O> {
 
     /**
      * Get the parser this one is derived from.
      *
      * @return the base parser
      */
-    @API(status = API.Status.STABLE)
-    public ArgumentParser<C, I> baseParser() {
-        return this.base;
-    }
-
-    @Override
-    public @NonNull CompletableFuture<@NonNull ArgumentParseResult<O>> parseFuture(
-            final @NonNull CommandContext<@NonNull C> commandContext,
-            final @NonNull CommandInput commandInput
-    ) {
-        return this.base.parseFuture(commandContext, commandInput)
-                .thenCompose(result -> this.mapper.map(commandContext, result));
-    }
-
-    @Override
-    public @NonNull SuggestionProvider<C> suggestionProvider() {
-        return this.base.suggestionProvider();
-    }
-
-    @Override
-    public <O1> ArgumentParser.@NonNull FutureArgumentParser<C, O1> flatMap(final Mapper<C, O, O1> mapper) {
-        requireNonNull(mapper, "mapper");
-        return new MappedArgumentParser<>(
-                this.base,
-                (ctx, orig) -> this.mapper.map(ctx, orig)
-                        .thenCompose(mapped -> mapper.map(ctx, mapped))
-        );
-    }
-
-    @Override
-    public int hashCode() {
-        return 31 + this.base.hashCode()
-                + 7 * this.mapper.hashCode();
-    }
-
-    @Override
-    public boolean equals(final @Nullable Object other) {
-        if (!(other instanceof MappedArgumentParser<?, ?, ?>)) {
-            return false;
-        }
-
-        final MappedArgumentParser<?, ?, ?> that = (MappedArgumentParser<?, ?, ?>) other;
-        return this.base.equals(that.base)
-                && this.mapper.equals(that.mapper);
-    }
-
-    @Override
-    public String toString() {
-        return "MappedArgumentParser{"
-                + "base=" + this.base + ','
-                + "mapper=" + this.mapper + '}';
-    }
-
+    @NonNull ArgumentParser<C, I> baseParser();
 
     @FunctionalInterface
-    @API(status = API.Status.STABLE)
-    public interface Mapper<C, I, O> {
+    interface Mapper<C, I, O> {
 
         /**
          * Maps the input to a future that completes with the output.
