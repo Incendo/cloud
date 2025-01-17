3379f44b66b6648b1f4d78f8a8d73f8203f7dd85
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2025-01-16 21:40:44 -0700

    Add dependency on BOM to all published modules (except bom itself)
    See https://blog.gradle.org/alignment-with-gradle-module-metadata

--- gradle/build-logic/src/main/kotlin/CloudPublishingConventions.kt
+++ gradle/build-logic/src/main/kotlin/CloudPublishingConventions.kt
@@ -1,13 +1,22 @@
 import net.kyori.indra.IndraExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPlugin
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.project
 import org.incendo.cloudbuildlogic.city
 import org.incendo.cloudbuildlogic.jmp
 
 class CloudPublishingConventions : Plugin<Project> {
     override fun apply(target: Project) {
         target.plugins.apply("org.incendo.cloud-build-logic.publishing")
+
+        if (!target.name.endsWith("-bom")) {
+            target.dependencies {
+                JavaPlugin.API_CONFIGURATION_NAME(platform(project(":cloud-bom")))
+            }
+        }
 
         target.extensions.configure(IndraExtension::class) {
             github("Incendo", "cloud") {
